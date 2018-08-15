c4efd3972f03e9e451b97b358639f75dd73ad7e6
Donna Jo Bridge <donnajb@gmail.com>
2018-08-15 14:35:31 -0500

    created animation function for brain data

--- eyepackage/make_brain_animation.py
+++ eyepackage/make_brain_animation.py
@@ -1,48 +1,70 @@
 import pandas as pd
 import numpy as np
+import matplotlib
+matplotlib.use("Agg")
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 
+
+# Set up formatting for the movie files
+Writer = animation.writers['ffmpeg']
+writer = Writer(fps=30, metadata=dict(artist='Me'), bitrate=1800)
+
+
 # load theta amplitude data here
-data = pd.read_csv('../data/theta_ev30.csv')
+trial = 30
+filestring = f'../data/theta_ev{trial}.csv'
+parts = filestring.split('_')
+reftrial = parts[1].split('.')[0]
+
+data = pd.read_csv(filestring)
 colmask = data.columns.str.contains('chan_3')
-data = data.loc[750:1250,:]
 
 data = data.loc[:,colmask]
+data = data.loc[750:1250,:]
 cols = data.columns.tolist()
 
-
-data = data['chan_3ev__1']
-data = data.reset_index()
-
-fig = plt.figure(figsize=(10, 4))
-
-x = data['index']
-y = data['chan_3ev__1']
-
-plt.axvline(x=1002)
-ax1 = fig.add_subplot(1,1,1)
-# ax2 = fig.add_subplot(4,1,2, aspect=1080/1920)
-# ax3 = fig.add_subplot(4,1,3, aspect=1080/1920)
-# ax4 = fig.add_subplot(4,1,4, aspect=1080/1920)
-
-
-ax1.set_xlim(750, 1250)
-ax1.set_ylim(-50, 50)
-ax1.set_title('Brain activity linked to viewing original location')
-ax1.axes.get_xaxis().set_visible(False)
-ax1.axes.get_yaxis().set_visible(False)
-
-
-line, = ax1.plot([], [], 'k-', alpha=0.5)
-dot, = ax1.plot([], [], 'ko', alpha=.5)
+x = data.index
+ys = []
+for num,item in enumerate(data.columns.tolist()):
+    y = f'y{num}'
+    ys.append(data[item])
 
 def update_line(num, x,y, line, dot):
     line.set_data(x[:num], y[:num])
     dot.set_data(x[num-1:num], y[num-1:num])
     return line, dot,
 
+fig, axs = plt.subplots(nrows=data.shape[1], sharex=True, sharey=True, ncols=1,figsize=(10,10))
+plt.xlabel('Time (ms)')
 
-line_ani = animation.FuncAnimation(fig, update_line, len(data), fargs=(x,y, line, dot),
-                                   interval=1, blit=True)
+lines = []
+dots = []
+for ax in axs.flat:
+    ax.axvline(x=1001)
+    ax.set_xlim(750, 1250)
+    ax.set_ylim(-40, 40)
+    ax.set_xticks([750, 875, 1001, 1125, 1250])
+    ax.set_xticklabels(['-500', '-250', '0', '250', '500'])
+    ax.set_ylabel('Amplitude (mV)')
+    ax.axvspan(825, 975, facecolor='b', alpha=0.1)
+    line, = ax.plot([], [], 'k-', alpha=0.5)
+    dot, = ax.plot([], [], 'ko', alpha=.5)
+
+    lines.append(line)
+    dots.append(dot)
+
+
+
+def update_lines(num, x, ys, lines, dots):
+    out = ()
+    for y, line, dot in zip(ys, lines, dots):
+        tmpout = update_line(num, x=x, y=y, line=line, dot=dot)
+        out = out + tmpout
+    return out
+
+line_ani = animation.FuncAnimation(fig, update_lines, len(data),
+                                   fargs=(x, ys, lines, dots), interval=1, blit=True)
+plt.tight_layout()
 plt.show()
+line_ani.save('../figs/theta_reftr'+reftrial+'.mp4', writer=writer)
