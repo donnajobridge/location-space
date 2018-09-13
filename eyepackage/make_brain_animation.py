caccabf06663d411e2fad667cfee26604d3f7952
donnajobridge <donnajb@gmail.com>
2018-09-12 22:22:57 -0500

    completed readme

--- eyepackage/make_brain_animation.py
+++ eyepackage/make_brain_animation.py
@@ -5,64 +5,69 @@ matplotlib.use("Agg")
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 
-
-# Set up formatting for the movie files
-Writer = animation.writers['imagemagick']
-writer = Writer(fps=50, metadata=dict(artist='Me'), bitrate=1800)
-
-
-# load theta amplitude data here
-trial = str(74)
-filestring = f'../data/theta_ev{trial}.csv'
-
-data = pd.read_csv(filestring)
-colmask = data.columns.str.contains('chan_3')
-
-data = data.loc[:,colmask]
-data = data.loc[750:1250,:]
-cols = data.columns.tolist()
-
-x = data.index
-ys = []
-for num,item in enumerate(data.columns.tolist()):
-    y = f'y{num}'
-    ys.append(data[item])
-
-def update_line(num, x,y, line, dot):
-    line.set_data(x[:num], y[:num])
-    dot.set_data(x[num-1:num], y[num-1:num])
-    return line, dot,
-
-fig, axs = plt.subplots(nrows=data.shape[1], sharex=True, sharey=True, ncols=1,figsize=(10,10))
-plt.xlabel('Time (ms)')
-
-lines = []
-dots = []
-for ax in axs.flat:
-    ax.axvline(x=1001)
-    ax.set_xlim(750, 1250)
-    ax.set_ylim(-40, 40)
-    ax.set_xticks([750, 875, 1001, 1125, 1250])
-    ax.set_xticklabels(['-500', '-250', '0', '250', '500'])
-    ax.set_ylabel('Amplitude (mV)')
-    ax.axvspan(825, 975, facecolor='b', alpha=0.1)
-    line, = ax.plot([], [], 'k-', alpha=0.5)
-    dot, = ax.plot([], [], 'ko', alpha=.5)
-
-    lines.append(line)
-    dots.append(dot)
+def make_thetawave_gif(sub,trialnum):
+    # Set up formatting for the movie files
+    Writer = animation.writers['imagemagick']
+    writer = Writer(fps=50, metadata=dict(artist='Me'), bitrate=1800)
 
 
+    # load theta amplitude data here
+    trial = str(trialnum)
+    filestring = f'../raw_ieeg_data/{sub}_5hz_ev{trial}.csv'
 
-def update_lines(num, x, ys, lines, dots):
-    out = ()
-    for y, line, dot in zip(ys, lines, dots):
-        tmpout = update_line(num, x=x, y=y, line=line, dot=dot)
-        out = out + tmpout
-    return out
+    data = pd.read_csv(filestring)
+    colmask = data.columns.str.contains('chan_3')
 
-line_ani = animation.FuncAnimation(fig, update_lines, len(data),
-                                   fargs=(x, ys, lines, dots), interval=1, blit=True)
-plt.tight_layout()
-plt.show()
-line_ani.save('../figs/theta_reftr'+trial+'50fps.gif', writer=writer)
+    data = data.loc[:,colmask]
+    data = data.loc[750:1250,:]
+    cols = data.columns.tolist()
+
+    x = data.index
+    ys = []
+    for num,item in enumerate(data.columns.tolist()):
+        y = f'y{num}'
+        ys.append(data[item])
+
+    def update_line(num, x,y, line, dot):
+        line.set_data(x[:num], y[:num])
+        dot.set_data(x[num-1:num], y[num-1:num])
+        return line, dot,
+
+    fig, axs = plt.subplots(nrows=data.shape[1], sharex=True, sharey=True, ncols=1,figsize=(10,10))
+    plt.xlabel('Time (ms)')
+
+    lines = []
+    dots = []
+    for ax in axs.flat:
+        ax.axvline(x=1001)
+        ax.set_xlim(750, 1250)
+        ax.set_ylim(-40, 40)
+        ax.set_xticks([750, 875, 1001, 1125, 1250])
+        ax.set_xticklabels(['-500', '-250', '0', '250', '500'])
+        ax.set_ylabel('Amplitude (mV)')
+        ax.axvspan(825, 975, facecolor='b', alpha=0.1)
+        line, = ax.plot([], [], 'k-', alpha=0.5)
+        dot, = ax.plot([], [], 'ko', alpha=.5)
+
+        lines.append(line)
+        dots.append(dot)
+
+
+
+    def update_lines(num, x, ys, lines, dots):
+        out = ()
+        for y, line, dot in zip(ys, lines, dots):
+            tmpout = update_line(num, x=x, y=y, line=line, dot=dot)
+            out = out + tmpout
+        return out
+
+    line_ani = animation.FuncAnimation(fig, update_lines, len(data),
+                                       fargs=(x, ys, lines, dots), interval=1, blit=True)
+    plt.tight_layout()
+    plt.show()
+    line_ani.save('../figs/theta_' + sub + '_reftr'+ trial + '_50fps.gif', writer=writer)
+
+make_thetawave_gif('ec108',30)
+print('finished gif')
+make_thetawave_gif('ec108',74)
+print('finished gif')
--- eyepackage/make_eyepath_gif_3phases.py
+++ eyepackage/make_eyepath_gif_3phases.py
@@ -6,139 +6,141 @@ import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 from imageio import imread
 
-# Set up formatting for the movie files
-Writer = animation.writers['imagemagick']
-writer = Writer(fps=5, metadata=dict(artist='Me'), bitrate=1800)
+def make_eyepath_3_phases(sub, ref_trial_num):
+    # Set up formatting for the movie files
+    Writer = animation.writers['imagemagick']
+    writer = Writer(fps=5, metadata=dict(artist='Me'), bitrate=1800)
 
 
-sub='ec108'
-phases=['study', 'refresh', 'recog']
-ref_trial_num = 30
+    phases=['study', 'refresh', 'recog']
 
-df_dict={}
-for phase in phases:
-    file='../data/' + sub + phase + 'eyebehave.csv'
-    df_dict[phase]=pd.read_csv(file, index_col=0)
+    df_dict={}
+    for phase in phases:
+        file='../data/' + sub + phase + 'eyebehave.csv'
+        df_dict[phase]=pd.read_csv(file, index_col=0)
 
 
-study_all = df_dict['study']
-refresh_all = df_dict['refresh']
-recog_all = df_dict['recog']
+    study_all = df_dict['study']
+    refresh_all = df_dict['refresh']
+    recog_all = df_dict['recog']
 
-study = study_all[(study_all['sub']==sub) & (study_all['refresh order']==ref_trial_num) ]
-refresh = refresh_all[(refresh_all['sub']==sub) & (refresh_all['refresh order']==ref_trial_num) ]
-recog = recog_all[(recog_all['sub']==sub) & (recog_all['refresh order']==ref_trial_num) ]
+    study = study_all[(study_all['sub']==sub) & (study_all['refresh order']==ref_trial_num) ]
+    refresh = refresh_all[(refresh_all['sub']==sub) & (refresh_all['refresh order']==ref_trial_num) ]
+    recog = recog_all[(recog_all['sub']==sub) & (recog_all['refresh order']==ref_trial_num) ]
 
-apple = imread('../figs/apple.png')
-grid = imread('../figs/grid.png')
-apple2 = apple[::-1]
-
-
-fig = plt.figure(figsize=(10, 4))
-
-ax1 = fig.add_subplot(1,3,1, aspect=1080/1920)
-ax2 = fig.add_subplot(1,3,2, aspect=1080/1920)
-ax3 = fig.add_subplot(1,3,3, aspect=1080/1920)
-
-ax1.set_xlim(0, 1920)
-ax1.set_ylim(0, 1080)
-ax1.set_title('Study', fontsize=20)
-ax1.axes.get_xaxis().set_visible(False)
-ax1.axes.get_yaxis().set_visible(False)
-
-ax2.set_xlim(0, 1920)
-ax2.set_ylim(0, 1080)
-ax2.set_title('Mismatch', fontsize=20)
-ax2.axes.get_xaxis().set_visible(False)
-ax2.axes.get_yaxis().set_visible(False)
-
-ax3.set_xlim(0, 1920)
-ax3.set_ylim(0, 1080)
-ax3.set_title('Recognition', fontsize=20)
-ax3.axes.get_xaxis().set_visible(False)
-ax3.axes.get_yaxis().set_visible(False)
-
-offset = 180
-
-# loc1 = {key: subdf[f'loc1{key}'].iloc[0] for key in ['x', 'y']}
-# loc2 = {key: subdf[f'loc2{key}'].iloc[0] for key in ['x', 'y']}
-
-# loc1_coords = [loc1['x']-offset, loc1['x']+offset, loc1['y']+offset, loc1['y']-offset]
-# loc2_coords = [loc2['x']-offset, loc2['x']+offset, loc2['y']+offset, loc2['y']-offset]
+    apple = imread('../figs/apple.png')
+    grid = imread('../figs/grid.png')
+    apple2 = apple[::-1]
 
 
-# for plotting apple
-def coords_to_borders(subdf, loc, offset):
-    xi = subdf[f'loc{loc}x'].iloc[0]
-    yi = subdf[f'loc{loc}y'].iloc[0]
-    return [xi-offset, xi+offset, yi+offset, yi-offset]
+    fig = plt.figure(figsize=(10, 4))
 
-loc1_coords = coords_to_borders(refresh, loc=1, offset=offset)
-loc2_coords = coords_to_borders(refresh, loc=2, offset=offset)
-loc3_coords = coords_to_borders(refresh, loc=3, offset=offset)
+    ax1 = fig.add_subplot(1,3,1, aspect=1080/1920)
+    ax2 = fig.add_subplot(1,3,2, aspect=1080/1920)
+    ax3 = fig.add_subplot(1,3,3, aspect=1080/1920)
 
-# plot grid
-ax1.imshow(grid, extent = [0, 1920, 0, 1080])
-ax2.imshow(grid, extent = [0, 1920, 0, 1080])
-ax3.imshow(grid, extent = [0, 1920, 0, 1080])
+    ax1.set_xlim(0, 1920)
+    ax1.set_ylim(0, 1080)
+    ax1.set_title('Study', fontsize=20)
+    ax1.axes.get_xaxis().set_visible(False)
+    ax1.axes.get_yaxis().set_visible(False)
 
-#plot apple
-# study
-ax1.imshow(apple2, extent = loc1_coords)
-# refresh
-ax2.imshow(apple2, extent = loc2_coords )
-# ax2.imshow(apple2_trans, extent = loc1_coords)
-# recog
-ax3.imshow(apple2, extent = loc1_coords )
-ax3.imshow(apple2, extent = loc2_coords )
-ax3.imshow(apple2, extent = loc3_coords )
+    ax2.set_xlim(0, 1920)
+    ax2.set_ylim(0, 1080)
+    ax2.set_title('Mismatch', fontsize=20)
+    ax2.axes.get_xaxis().set_visible(False)
+    ax2.axes.get_yaxis().set_visible(False)
 
-x_stu=study['xstart']
-y_stu=study['ystart']
-nstupoints = len(y_stu)
+    ax3.set_xlim(0, 1920)
+    ax3.set_ylim(0, 1080)
+    ax3.set_title('Recognition', fontsize=20)
+    ax3.axes.get_xaxis().set_visible(False)
+    ax3.axes.get_yaxis().set_visible(False)
+
+    offset = 180
+
+    # loc1 = {key: subdf[f'loc1{key}'].iloc[0] for key in ['x', 'y']}
+    # loc2 = {key: subdf[f'loc2{key}'].iloc[0] for key in ['x', 'y']}
+
+    # loc1_coords = [loc1['x']-offset, loc1['x']+offset, loc1['y']+offset, loc1['y']-offset]
+    # loc2_coords = [loc2['x']-offset, loc2['x']+offset, loc2['y']+offset, loc2['y']-offset]
 
 
-x_ref=refresh['xstart']
-y_ref=refresh['ystart']
-nrefpoints = len(y_ref)
+    # for plotting apple
+    def coords_to_borders(subdf, loc, offset):
+        xi = subdf[f'loc{loc}x'].iloc[0]
+        yi = subdf[f'loc{loc}y'].iloc[0]
+        return [xi-offset, xi+offset, yi+offset, yi-offset]
 
-x_rec=recog['xstart']
-y_rec=recog['ystart']
-nrecpoints = len(y_rec)
+    loc1_coords = coords_to_borders(refresh, loc=1, offset=offset)
+    loc2_coords = coords_to_borders(refresh, loc=2, offset=offset)
+    loc3_coords = coords_to_borders(refresh, loc=3, offset=offset)
 
-# extra_points = nrefpoints - nstupoints
-# x_stu = x_stu.append(x_stu)
-# x_stu = x_stu.append(x_stu)
-# y_stu = y_stu.append(y_stu)
-# y_stu = y_stu.append(y_stu)
-print(nstupoints, nrefpoints, nrecpoints)
+    # plot grid
+    ax1.imshow(grid, extent = [0, 1920, 0, 1080])
+    ax2.imshow(grid, extent = [0, 1920, 0, 1080])
+    ax3.imshow(grid, extent = [0, 1920, 0, 1080])
 
-maxpoints = max([nstupoints, nrefpoints, nrecpoints])
+    #plot apple
+    # study
+    ax1.imshow(apple2, extent = loc1_coords)
+    # refresh
+    ax2.imshow(apple2, extent = loc2_coords )
+    # ax2.imshow(apple2_trans, extent = loc1_coords)
+    # recog
+    ax3.imshow(apple2, extent = loc1_coords )
+    ax3.imshow(apple2, extent = loc2_coords )
+    ax3.imshow(apple2, extent = loc3_coords )
 
-l_stu, = ax1.plot([], [], 'w-', alpha=0.5)
-dot_stu, = ax1.plot([], [], 'wo', alpha=.5)
+    x_stu=study['xstart']
+    y_stu=study['ystart']
+    nstupoints = len(y_stu)
 
-l_ref, = ax2.plot([], [], 'w-', alpha=0.5)
-dot_ref, = ax2.plot([], [], 'wo', alpha=.5)
 
-l_rec, = ax3.plot([], [], 'w-', alpha=0.5)
-dot_rec, = ax3.plot([], [], 'wo', alpha=.5)
+    x_ref=refresh['xstart']
+    y_ref=refresh['ystart']
+    nrefpoints = len(y_ref)
 
-def update_line(num, x,y, line, dot):
-    i = num
-    if num > len(x):
-        i = len(x)
-    line.set_data(x[:i], y[:i])
-    dot.set_data(x[i-1:i], y[i-1:i])
-    return line, dot,
+    x_rec=recog['xstart']
+    y_rec=recog['ystart']
+    nrecpoints = len(y_rec)
 
-def update_lines(num, x_stu, y_stu, x_ref, y_ref, x_rec, y_rec, l_stu, dot_stu, l_ref, dot_ref, l_rec, dot_rec ):
-    a = update_line(num, x=x_stu, y=y_stu, line=l_stu, dot=dot_stu)
-    b = update_line(num, x=x_ref, y=y_ref, line=l_ref, dot=dot_ref)
-    c = update_line(num, x=x_rec, y=y_rec, line=l_rec, dot=dot_rec)
-    return a + b + c
+    # extra_points = nrefpoints - nstupoints
+    # x_stu = x_stu.append(x_stu)
+    # x_stu = x_stu.append(x_stu)
+    # y_stu = y_stu.append(y_stu)
+    # y_stu = y_stu.append(y_stu)
+    print(nstupoints, nrefpoints, nrecpoints)
 
-line_ani = animation.FuncAnimation(fig, update_lines, maxpoints,
-                                   fargs=(x_stu, y_stu, x_ref, y_ref, x_rec, y_rec, l_stu,
-                                          dot_stu, l_ref, dot_ref, l_rec, dot_rec), interval=200, blit=True)
-line_ani.save('../figs/eyepath_reftr'+str(ref_trial_num)+sub+'.gif', writer=writer)
+    maxpoints = max([nstupoints, nrefpoints, nrecpoints])
+
+    l_stu, = ax1.plot([], [], 'w-', alpha=0.5)
+    dot_stu, = ax1.plot([], [], 'wo', alpha=.5)
+
+    l_ref, = ax2.plot([], [], 'w-', alpha=0.5)
+    dot_ref, = ax2.plot([], [], 'wo', alpha=.5)
+
+    l_rec, = ax3.plot([], [], 'w-', alpha=0.5)
+    dot_rec, = ax3.plot([], [], 'wo', alpha=.5)
+
+    def update_line(num, x,y, line, dot):
+        i = num
+        if num > len(x):
+            i = len(x)
+        line.set_data(x[:i], y[:i])
+        dot.set_data(x[i-1:i], y[i-1:i])
+        return line, dot,
+
+    def update_lines(num, x_stu, y_stu, x_ref, y_ref, x_rec, y_rec, l_stu, dot_stu, l_ref, dot_ref, l_rec, dot_rec ):
+        a = update_line(num, x=x_stu, y=y_stu, line=l_stu, dot=dot_stu)
+        b = update_line(num, x=x_ref, y=y_ref, line=l_ref, dot=dot_ref)
+        c = update_line(num, x=x_rec, y=y_rec, line=l_rec, dot=dot_rec)
+        return a + b + c
+
+    line_ani = animation.FuncAnimation(fig, update_lines, maxpoints,
+                                       fargs=(x_stu, y_stu, x_ref, y_ref, x_rec, y_rec, l_stu,
+                                              dot_stu, l_ref, dot_ref, l_rec, dot_rec), interval=200, blit=True)
+    line_ani.save('../figs/eyepath_reftr'+str(ref_trial_num)+sub+'.gif', writer=writer)
+
+make_eyepath_3_phases('ec108', 30)
+make_eyepath_3_phases('ec108', 74)
